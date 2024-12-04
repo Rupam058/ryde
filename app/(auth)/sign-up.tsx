9a112c52d6562547b8b22205ac7f825ddfe921f9
Rupam058 <rupam.irp@gmail.com>
2024-12-04 11:38:45 +0530

    welcome screen

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center h-full">
+      <Text className="text-3xl text-red-400">SignUp</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;

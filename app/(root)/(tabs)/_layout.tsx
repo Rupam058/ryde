11b876eec1a6096bcad7b50158430d3056014ff6
Rupam058 <rupam.irp@gmail.com>
2024-12-11 11:30:12 +0530

    tab layout

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,10 +1,91 @@
-import { Stack } from "expo-router";
-import "react-native-reanimated";
+import { icons } from "@/constants";
+import { Tabs } from "expo-router";
+import { Image, ImageSourcePropType, View } from "react-native";
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
+const TabIcon = ({
+  source,
+  focused,
+}: {
+  source: ImageSourcePropType;
+  focused: boolean;
+}) => (
+  <View
+    className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-primary-300" : ""}`}>
+    <View
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-primary-400" : ""}`}>
+      <Image
+        source={source}
+        className="w-7 h-7"
+        tintColor="white"
+        resizeMode="contain"
+      />
+    </View>
+  </View>
+);
+
+const Layout = () => (
+  <Tabs
+    initialRouteName="home"
+    screenOptions={{
+      tabBarActiveTintColor: "white",
+      tabBarInactiveTintColor: "white",
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        backgroundColor: "#333333",
+        borderRadius: 50,
+        paddingBottom: 25,
+        overflow: "hidden",
+        marginHorizontal: 20,
+        marginBottom: 20,
+        height: 78,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        position: "absolute",
+      },
+    }}>
+    <Tabs.Screen
+      name="home"
+      options={{
+        title: "Home",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.home} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="rides"
+      options={{
+        title: "Rides",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.list} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="chat"
+      options={{
+        title: "Chat",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.chat} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        title: "Profile",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.profile} />
+        ),
+      }}
+    />
+  </Tabs>
+);
+
+export default Layout;
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,3 @@
+const Rides = () => {};
+
+export default Rides;
